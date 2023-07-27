fa35f053a06cc3ae6658180d304daa39184ceeb8
ishaqsk442 <skishaq456@gmail.com>
2023-07-27 15:20:01 +0000

    react context adding consumer

---
+++ src/components/FeaturesSection/index.js
@@ -0,0 +1,15 @@
+import Playtime from '../Playtime'
+import NewWaysToConnect from '../NewWaysToConnect'
+
+import './index.css'
+
+const FeaturesSection = () => (
+  //   const {activeLanguage} = props
+
+  <div className="features-section-container">
+    <Playtime />
+    <NewWaysToConnect />
+  </div>
+)
+
+export default FeaturesSection
